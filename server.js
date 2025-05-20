bac61a36b63fac87931df42b93529d6ed3f584f1
Nilsen <dmitry.nilsen@gmail.com>
2025-05-20 11:35:51 +0200

    readme

--- server.js
+++ server.js
@@ -8,38 +8,16 @@ const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
 const PORT = 3000;
-const BEARER_TOKEN = 'd1100ed3f46ee4be4073ae80c88fc2fe';
+const BEARER_TOKEN = 'your-turnix-api-token-here';
 
 const rooms = {}; // { roomId: Set<clients> }
 
 app.use(express.static('public'));
 app.use(express.json());
 
-
-//     // Filter each server's URLs to include only 'turns:' entries and change port 443 to 444
-//     const filteredServers = iceServers
-//         .map(server => {
-//             const filteredUrls = server.urls
-//                 .filter(url => url.startsWith('turns:'))
-//                 .map(url => url.replace(':443', ':444'));
-//
-//             if (filteredUrls.length > 0) {
-//                 return {
-//                     ...server,
-//                     urls: filteredUrls
-//                 };
-//             }
-//             return null;
-//         })
-//         .filter(Boolean); // remove nulls
-
-
-
-
 app.post('/ice', async (req, res) => {
     const turnix = new TurnixIO({
         bearerToken: BEARER_TOKEN,
-        apiUrl: "https://staging.turnix.io/api/v1/credentials/ice"
     });
 
     const iceServers = await turnix.requestCredentials({
